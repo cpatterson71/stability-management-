c7d956616e04e8b508057daeff95019c6aea112e
cpatterson71 <carl.pattersonsd@gmail.com>
2026-02-24 15:04:10 -0800

    feat: Implement packaging editor in PySide Tab 1

--- pyside_tabs/tab1_setup.py
+++ pyside_tabs/tab1_setup.py
@@ -9,9 +9,56 @@ from PySide6.QtWidgets import (
     QPushButton,
     QTableView,
     QLabel,
-    QHeaderView
+    QHeaderView,
+    QHBoxLayout
 )
-from PySide6.QtCore import QDate
+from PySide6.QtCore import QDate, Qt, QAbstractTableModel
+import pandas as pd
+
+class PackagingTableModel(QAbstractTableModel):
+    def __init__(self, data):
+        super().__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.ItemDataRole.DisplayRole or role == Qt.ItemDataRole.EditRole:
+            return self._data.iloc[index.row(), index.column()]
+
+    def setData(self, index, value, role):
+        if role == Qt.ItemDataRole.EditRole:
+            self._data.iloc[index.row(), index.column()] = value
+            self.dataChanged.emit(index, index)
+            return True
+        return False
+
+    def rowCount(self, index):
+        return len(self._data)
+
+    def columnCount(self, index):
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.ItemDataRole.DisplayRole:
+            if orientation == Qt.Orientation.Horizontal:
+                return str(self._data.columns[section])
+            if orientation == Qt.Orientation.Vertical:
+                return str(self._data.index[section])
+
+    def flags(self, index):
+        return Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsEditable
+
+    def insertRow(self, row, parent):
+        self.beginInsertRows(parent, row, row)
+        new_row = pd.DataFrame([["", "", ""]], columns=self._data.columns)
+        self._data = pd.concat([self._data.iloc[:row], new_row, self._data.iloc[row:]]).reset_index(drop=True)
+        self.endInsertRows()
+        return True
+
+    def removeRow(self, row, parent):
+        self.beginRemoveRows(parent, row, row)
+        self._data = self._data.drop(self._data.index[row]).reset_index(drop=True)
+        self.endRemoveRows()
+        return True
 
 class Tab1Setup(QWidget):
     def __init__(self, conn):
@@ -60,6 +107,44 @@ class Tab1Setup(QWidget):
 
         self.layout().addLayout(form_layout)
 
-        # Placeholder for the rest of the UI
-        self.layout().addWidget(QLabel("More UI components to be added here..."))
+        # Packaging Editor
+        self.setup_packaging_editor()
 
+    def setup_packaging_editor(self):
+        packaging_groupbox = QGroupBox("Packaging (primary only)")
+        packaging_layout = QVBoxLayout()
+
+        self.packaging_table = QTableView()
+        packaging_data = pd.DataFrame([
+            ["", "", ""],
+            ["", "", ""]
+        ], columns=["Supplier Part Number", "Description", "Supplier"])
+        self.packaging_model = PackagingTableModel(packaging_data)
+        self.packaging_table.setModel(self.packaging_model)
+        self.packaging_table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        
+        packaging_layout.addWidget(self.packaging_table)
+
+        # Buttons for adding/removing rows
+        button_layout = QHBoxLayout()
+        add_row_button = QPushButton("Add Packaging")
+        add_row_button.clicked.connect(self.add_packaging_row)
+        remove_row_button = QPushButton("Remove Selected Packaging")
+        remove_row_button.clicked.connect(self.remove_packaging_row)
+        button_layout.addWidget(add_row_button)
+        button_layout.addWidget(remove_row_button)
+        packaging_layout.addLayout(button_layout)
+
+        packaging_groupbox.setLayout(packaging_layout)
+        self.layout().addWidget(packaging_groupbox)
+    
+    def add_packaging_row(self):
+        self.packaging_model.insertRow(self.packaging_model.rowCount(), self.packaging_table.rootIndex())
+
+    def remove_packaging_row(self):
+        selected_rows = self.packaging_table.selectionModel().selectedRows()
+        if not selected_rows:
+            return
+        # Remove rows in reverse order to avoid index shifting issues
+        for index in sorted(selected_rows, reverse=True):
+            self.packaging_model.removeRow(index.row(), self.packaging_table.rootIndex())
